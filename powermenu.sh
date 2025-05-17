0c0cd159ed72998dbd11087946438bf0748f5106
Jakob Holzinger <jakob.holzinger@tutanota.de>
2025-05-17 19:59:47 +0200

    translate powermenu

--- powermenu.sh
+++ powermenu.sh
@@ -3,8 +3,8 @@
 choice=$(printf "Sperren\nAbmelden\nNeustarten\nHerunterfahren" | dmenu -i -p "System:")
 
 case "$choice" in
-  Sperren) slock ;;
-  Abmelden) pkill -KILL -u "$USER" ;;
-  Neustarten) systemctl reboot ;;
-  Herunterfahren) systemctl poweroff ;;
+  Lock) slock ;;
+  Log Out) pkill -KILL -u "$USER" ;;
+  Restart) systemctl reboot ;;
+  Shutdown) systemctl poweroff ;;
 esac
