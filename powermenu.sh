5fcd0ce77dc472f89363f3b598b91430c291a96e
Jakob Holzinger <jakob.holzinger@tutanota.de>
2025-05-31 12:49:37 +0200

    add suspend to powermenu

--- powermenu.sh
+++ powermenu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-choice=$(printf "Lock\nLogOut\nRestart\nShutdown" | dmenu -i -p "System:")
+choice=$(printf "Lock\nLogOut\nSuspend\nRestart\nShutdown" | dmenu -i -p "System:")
 
 case "$choice" in
   Lock) slock ;;
   LogOut) pkill -KILL -u "$USER" ;;
+  Suspend) systemctl suspend ;;
   Restart) systemctl reboot ;;
   Shutdown) systemctl poweroff ;;
 esac
